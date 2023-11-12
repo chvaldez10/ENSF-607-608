8cb51869fbe8b1f348309ba83e40d20e78ee5e85
chvaldez10 <christianvaldez021@gmail.com>
2023-11-12 10:02:10 -0700

    finished intro

--- Learn Spring Boot/learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ Learn Spring Boot/learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -3,14 +3,18 @@ package com.in28minutes.springboot.learnspringboot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+	
+	@RequestMapping("/courses")
 	public List<Course> retreiveAllCourses() {
 		return Arrays.asList(
 				new Course(1, "Learn AWS", "in28min"),
-				new Course(1, "Learn DevOps", "in28min")
+				new Course(2, "Learn DevOps", "in28min"),
+				new Course(3, "Learn Azure", "in28min")
 				);
 	}
 }
