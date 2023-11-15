9aeaf05beb10e888378421967bd546cc617f9b8d
chvaldez10 <christianvaldez021@gmail.com>
2023-11-15 08:47:49 -0700

    finished counter app

---
+++ Full Stack/todo-app/src/components/todo/TodoApp.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+import "./TodoApp.css";
+
+export default function TodoApp() {
+  return (
+    <div className="TodoApp">
+      Todo Management Application
+      <LoginComponent />
+      <WelcomeComponent />
+    </div>
+  );
+}
+
+function LoginComponent() {
+  const [username, setUsername] = useState("in28minutes");
+
+  function handleUsernameChange(event) {
+    console.log(event);
+  }
+
+  return (
+    <div className="Login">
+      <div className="LoginForm">
+        <div>
+          <label>User Name:</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <button type="button" name="login">
+            login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function WelcomeComponent() {
+  return <div className="Welcome">Welcom Component</div>;
+}
