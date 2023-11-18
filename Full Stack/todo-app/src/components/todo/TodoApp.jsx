3f8c1efca98c429cccb2b4fb4ca714450e503fb5
chvaldez10 <christianvaldez021@gmail.com>
2023-11-18 11:48:53 -0700

    added

--- Full Stack/todo-app/src/components/todo/TodoApp.jsx
+++ Full Stack/todo-app/src/components/todo/TodoApp.jsx
@@ -1,84 +1,73 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LogoutComponent from "./LogoutComponent";
+import HeaderComponent from "./HeaderComponent";
+import ListTodosComponent from "./ListTodosComponent";
+import ErrorComponent from "./ErrorComponent";
+import WelcomeComponent from "./WelcomeComponent";
+import LoginComponent from "./LoginComponent";
+import TodoComponent from "./TodoComponent";
+import AuthProvider, { useAuth } from "./security/AuthContext";
 
 import "./TodoApp.css";
+
+function AuthenticatedRoute({ children }) {
+  const authContext = useAuth();
+
+  if (authContext.isAuthenticated) return children;
+
+  return <Navigate to="/" />;
+}
 
 export default function TodoApp() {
   return (
     <div className="TodoApp">
-      Todo Management Application
-      <LoginComponent />
-      <WelcomeComponent />
+      <AuthProvider>
+        <BrowserRouter>
+          <HeaderComponent />
+          <Routes>
+            <Route path="/" element={<LoginComponent />} />
+            <Route path="/login" element={<LoginComponent />} />
+
+            <Route
+              path="/welcome/:username"
+              element={
+                <AuthenticatedRoute>
+                  <WelcomeComponent />
+                </AuthenticatedRoute>
+              }
+            />
+
+            <Route
+              path="/todos"
+              element={
+                <AuthenticatedRoute>
+                  <ListTodosComponent />
+                </AuthenticatedRoute>
+              }
+            />
+
+            <Route
+              path="/todo/:id"
+              element={
+                <AuthenticatedRoute>
+                  <TodoComponent />
+                </AuthenticatedRoute>
+              }
+            />
+
+            <Route
+              path="/logout"
+              element={
+                <AuthenticatedRoute>
+                  <LogoutComponent />
+                </AuthenticatedRoute>
+              }
+            />
+
+            <Route path="*" element={<ErrorComponent />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
-}
-
-function LoginComponent() {
-  const [username, setUsername] = useState("in28minutes");
-  const [password, setPassword] = useState("");
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [showErrorMessage, setShowErrorMessage] = useState(false);
-
-  function handleUsernameChange(event) {
-    console.log(event.target.value);
-    setUsername(event.target.value);
-  }
-
-  function handlePasswordChange(event) {
-    console.log(event.target.value);
-    setPassword(event.target.value);
-  }
-
-  function handleSubmit() {
-    if (username === "in28minutes" && password === "dummy") {
-      setShowSuccessMessage(true);
-      setShowErrorMessage(false);
-    } else {
-      setShowSuccessMessage(false);
-      setShowErrorMessage(true);
-    }
-  }
-
-  function SucessMessageComponent() {
-    if (showSuccessMessage) return <div>Successful Authentication</div>;
-  }
-
-  function ErrorMessageComponent() {
-    if (showErrorMessage) return <div>Error</div>;
-  }
-
-  return (
-    <div className="Login">
-      <SucessMessageComponent />
-      <ErrorMessageComponent />
-      <div className="LoginForm">
-        <div>
-          <label>User Name:</label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div>
-          <button type="button" name="login" onClick={handleSubmit}>
-            login
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function WelcomeComponent() {
-  return <div className="Welcome">Welcom Component</div>;
 }
